2d7eb8bfb8af2f236c5a2f6fb3e68b03ba83ad1c
Mohit <devmohit93@gmail.com>
2022-08-22 17:34:14 +0530

    fix: reverted changes

--- src/Components/CoffeesData.jsx
+++ src/Components/CoffeesData.jsx
@@ -52,6 +52,9 @@ export const CoffeesData = () => {
             </div>
           </div>
         </div>
+        <div className="flex items-center justify-center">
+          {data.length < 1 ? <p className="mt-10">Please Wait Loading Data...</p> : ""}
+        </div>
         {[...data]
           .slice(0, 5)
           .reverse()
